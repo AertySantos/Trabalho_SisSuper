498382981520111649f8c875f5863f64bf54b604
aertyps <aertyps@hotmail.com>
2021-11-06 20:48:48 -0300

    classes elevador e botao

---
+++ Botao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_SisSuper
+{
+    class Botao
+    {
+        private bool acao;
+        private bool estado;
+        public Botao()
+        {
+            SetAcao(false);
+            SetEstado(false);
+        }
+
+        public void SetAcao(bool a)
+        {
+            this.acao = a;
+        }
+
+        public void SetEstado(bool e)
+        {
+            this.estado = e;
+        }
+
+        public bool GetAcao()
+        {
+            return this.acao;
+        }
+
+        public bool GetEstado()
+        {
+            return this.estado;
+        }
+    }
+}
---
+++ Elevador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_SisSuper
+{
+    class Elevador
+    {
+        private bool modo;//automatico ou manual
+        private int andar;//onde esta o elevador
+
+        List<Botao> bt = new List<Botao>();//lista de botoes
+
+        public Elevador(int num, bool md, int a)
+        {
+
+            this.modo = md;
+            this.andar = a;
+            Botao bts = new Botao();
+            int contador = num;
+
+            while (contador > 0)
+            {
+                bt.Add(bts);//gera uma lista de botoes
+                contador--;
+            }
+
+        }
+
+        public void SetModo(bool m)
+        {
+            this.modo = m;
+        }
+
+        public void SetAndar(int a)
+        {
+            this.andar = a;
+        }
+
+        public bool GetModo()
+        {
+            return this.modo;
+        }
+
+        public int GetAndar()
+        {
+            return this.andar;
+        }
+    }
+
+}
